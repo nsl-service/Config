e274c0f47bb6b2f7e02f5c9db2579869d01a4186
suwathi.s <suwathi.s@EXIND3020-18.allfon.in>
2020-07-24 12:11:56 +0530

    property commit

--- src/test/java/com/nsl/ServerConfigApplicationTests.java
+++ src/test/java/com/nsl/ServerConfigApplicationTests.java
@@ -1,9 +1,13 @@
 package com.nsl;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootTest
+@EnableAutoConfiguration
+@EnableConfigServer
 class ServerConfigApplicationTests {
 
 	@Test
