77032bd518203a75daff6f697c99fe09bf8ad0ed
suwathi.s <suwathi.s@EXIND3020-18.allfon.in>
2020-07-24 11:29:36 +0530

    Initial Commit

---
+++ src/main/java/com/nsl/ServerConfigApplication.java
@@ -0,0 +1,16 @@
+package com.nsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ServerConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerConfigApplication.class, args);
+		System.out.println("ServerConfigApplication");
+	}
+
+}
